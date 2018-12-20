4305baad9b5667c5324c6924efa834d7fe9c2608
Prerana Sunilkumar <preranasunilkumar@preranas-macbook-pro.local>
2018-12-20 15:08:08 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Prerana Sunilkumar on 12/20/18.
+//  Copyright © 2018 Prerana Sunilkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
